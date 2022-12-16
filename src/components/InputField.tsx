37f4a9c390f1a1f13baa8c2b4bd4bedd862060df
hbohlen <bohlenhayden@gmail.com>
2022-12-16 17:24:03 -0600

    Created event handler for adding tasks

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React from 'react';
+import './styles.css';
 
-const InputField = () => {
-  return <div>InputField</div>;
+interface Props {
+  todo: string | undefined;
+  setTodo: React.Dispatch<React.SetStateAction<string | undefined>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  return (
+    <form className="input" onSubmit={handleAdd}>
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+      />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
 };
 
 export default InputField;
